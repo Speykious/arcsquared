bddc83ecc8a479c60cb8e380a346f4ab39e6dc11
Speykious <speykious@gmail.com>
2021-02-02 17:52:33 +0100

    The functional-est (and only) helper so far

--- src/helpers.ts
+++ src/helpers.ts
@@ -7,3 +7,8 @@ export type TypedArray
   | Int32Array
   | Float32Array
   | Float64Array
+
+/** If the condition is true, it will return the application of f to arg. Otherwise, it just returns arg. */
+export const optionalTransform =
+  (condition: boolean) => <T>(f: (arg: T) => T) => (arg: T) =>
+    condition ? f(arg) : arg;
