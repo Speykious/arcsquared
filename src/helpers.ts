ea4e52770d9d07afb94e89dd3955c5c767729bb6
Speykious <speykious@gmail.com>
2021-02-03 22:53:06 +0100

    Adding the compiled regexs

--- src/helpers.ts
+++ src/helpers.ts
@@ -12,3 +12,10 @@ export type TypedArray
 export const optionalTransform =
   (condition: boolean) => <T>(f: (arg: T) => T) => (arg: T) =>
     condition ? f(arg) : arg;
+
+// Caching compiled regexs for better performance
+export const reDigit = /[0-9]/;
+export const reDigits = /^[0-9]+/;
+export const reLetter = /[a-zA-Z]/;
+export const reLetters = /^[a-zA-Z]+/;
+export const reWhitespaces = /^\s+/;
