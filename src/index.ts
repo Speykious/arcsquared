c4457ce019f44f6467e24dada4d236ef5ea0fe63
Speykious <speykious@gmail.com>
2021-02-03 22:43:08 +0100

    Just a generator function that I wrote to test something

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ export const sum = (a: number, b: number) => {
   console.log("yeet²");
   return a + b;
 };
+
+export function* foo() {
+  let bool: boolean = true;
+  if (Math.random() < 0.5)
+    bool = yield 100;
+  return `Finished! (${bool})`;
+}
