7e0074108b9605d111eb810725acd4eb51448908
Speykious <speykious@gmail.com>
2021-02-13 00:49:04 +0100

    Formatting and using UEOF

--- test/ParsingError.test.ts
+++ test/ParsingError.test.ts
@@ -1,8 +1,8 @@
-import { EOF, ParsingError } from "../src/index";
+import { UEOF, ParsingError } from "../src/index";
 import { bold, green, red } from "ansi-colors-ts";
 
 describe("ParsingError", () => {
-  it("correctly uses colors", () => {
+  it("correctly displays", () => {
     const perr = new ParsingError({
       index: 0,
       from: "someParserCombinator",
@@ -10,30 +10,34 @@ describe("ParsingError", () => {
       actual: "42",
       colored: true
     });
-    
-    expect(perr.toString())
-    .toBe(`${red(
-      `${bold("ParsingError")} 'someParserCombinator' (position 0): `
-    )}Expected ${green("1")}, got ${red("42")}`);
-    
+
+    expect(perr.toString()).toBe(
+      `${red(
+        `${bold("ParsingError")} 'someParserCombinator' (position 0): `
+      )}Expected ${green("1")}, got ${red("42")}`
+    );
+
     perr.colored = false;
-    expect(perr.toString())
-    .toBe(`ParsingError 'someParserCombinator' (position 0): Expected 1, got 42`);
-    
+    expect(perr.toString()).toBe(
+      `ParsingError 'someParserCombinator' (position 0): Expected 1, got 42`
+    );
+
     const perr2 = new ParsingError({
       index: 69,
       from: "someOtherParserCombinator",
-      message: `Unexpected ${EOF}`,
+      message: UEOF,
       colored: true
     });
-    
-    expect(perr2.toString())
-    .toBe(`${red(
-      `${bold("ParsingError")} 'someOtherParserCombinator' (position 69): `
-    )}Unexpected ${EOF}`);
-    
+
+    expect(perr2.toString()).toBe(
+      `${red(
+        `${bold("ParsingError")} 'someOtherParserCombinator' (position 69): `
+      )}${UEOF}`
+    );
+
     perr2.colored = false;
-    expect(perr2.toString())
-    .toBe(`ParsingError 'someOtherParserCombinator' (position 69): Unexpected ${EOF}`);
+    expect(perr2.toString()).toBe(
+      `ParsingError 'someOtherParserCombinator' (position 69): ${UEOF}`
+    );
   });
 });
