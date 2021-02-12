97c8a8c344b80346e2fe8a345db21bb568911b2e
Speykious <speykious@gmail.com>
2021-02-12 17:03:37 +0100

    Finally removed that useless stuff

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-export const sum = (a: number, b: number) => {
-  console.log("yeet²");
-  return a + b;
+import PStream from "./PStream";
+import StringPStream from "./StringPStream";
+import ParsingError from "./ParsingError";
+import ParserState from "./ParserState";
+import Parser from "./Parser";
+
+export * from "./PStream";
+export * from "./StringPStream";
+export * from "./ParsingError";
+export * from "./ParserState";
+export * from "./Parser";
+export * from "./pcombinators";
+export * from "./pgenerators";
+
+export {
+  PStream,
+  StringPStream,
+  ParsingError,
+  ParserState,
+  Parser
 };
 
-export function* foo() {
-  let bool: boolean = true;
-  if (Math.random() < 0.5)
-    bool = yield 100;
-  return `Finished! (${bool})`;
-}
+export default Parser;
