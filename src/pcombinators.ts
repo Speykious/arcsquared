698fabd27c42d9dab4f7a0e98cce7e614960eba0
Speykious <speykious@gmail.com>
2021-02-15 00:32:40 +0100

    Added intersperse, sep and sep1 parser combinators

--- src/pcombinators.ts
+++ src/pcombinators.ts
@@ -6,7 +6,8 @@ import ParserState from "./ParserState";
 import ParsingError from "./ParsingError";
 //import ParsingError from "./ParsingError";
 
-/** A parser that will always return what is contained in the *internal state data*, without consuming any input.
+/**
+ * A parser that will always return what is contained in the *internal state data*, without consuming any input.
  *
  * If dealing with any complex level of state ─ such as an object where individual keys will be updated or required ─ then it can be useful to create utility parsers to assist.
  */
@@ -14,7 +15,8 @@ export const getData = new Parser(s =>
   s.error ? s : s.resultify(s.data)
 );
 
-/** Takes anything that should be set as the *internal state data*, and returns a parser that will perform that side effect when the parser is run. This does not consume any input. If parsing is currently in an errored state, then the data **will not** be set.
+/**
+ * Takes anything that should be set as the *internal state data*, and returns a parser that will perform that side effect when the parser is run. This does not consume any input. If parsing is currently in an errored state, then the data **will not** be set.
  * 
  * If dealing with any complex level of state - such as an object where individual keys will be updated or required, then it can be useful to create utility parsers to assist with updating the *internal state data*.
  */
@@ -27,8 +29,7 @@ export const setData = <T extends PStream<any>, D, R>(d: D) =>
 export const withData = <T extends PStream<any>, D, R>(parser: Parser<T, D, R>) => (d: D) =>
   setData<T, D, R>(d).chain(() => parser);
 
-/** Takes an array of parsers and composes them left to right, so each parser's return value is passed into the next one in the chain. The result is a new parser that, when run, yields the result of the final parser in the chain.
- */
+/** Takes an array of parsers and composes them left to right, so each parser's return value is passed into the next one in the chain. The result is a new parser that, when run, yields the result of the final parser in the chain. */
 export const pipe = <T extends PStream<any>, D, R extends any[]>(parsers: ParserTuple<T, D, R>) =>
   new Parser(s => {
     for (const parser of parsers)
@@ -36,8 +37,7 @@ export const pipe = <T extends PStream<any>, D, R extends any[]>(parsers: Parser
     return s;
   }) as Parser<T, D, R[typeof parsers.length]>;
 
-/** Takes an array of parsers and composes them right to left, so each parsers return value is passed into the next one in the chain. The result is a new parser that, when run, yields the result of the final parser in the chain.
- */
+/** Takes an array of parsers and composes them right to left, so each parsers return value is passed into the next one in the chain. The result is a new parser that, when run, yields the result of the final parser in the chain. */
 export const compose = <T extends PStream<any>, D, R extends any[]>(parsers: ParserTuple<T, D, R>) =>
   pipe([...parsers].reverse() as ParserTuple<T, D, R>) as Parser<T, D, R[0]>;
 
@@ -61,7 +61,8 @@ export const tap = <T extends PStream<any>, D, R>(f: (state: ParserState<T, D, R
     return s;
   }) as Parser<T, D, R>;
 
-/** Functional wrapper around the `.parse()` method of `Parser`.
+/**
+ * Functional wrapper around the `.parse()` method of `Parser`.
  * 
  * Takes a parser, and returns a function that takes some `PStream` input to parse it using that parser.
  * @returns the parser state resulting from that parsing.
@@ -109,7 +110,8 @@ export const either = <T extends PStream<any>, D, R>(parser: Parser<T, D, R>) =>
     return state.resultify(state.result ?? state.error);
   }) as Parser<T, D, R>;
 
-/** Takes a generator function, in which parsers are `yield`ed. `coroutine` allows you to write parsers in a more imperative and sequential way ─ in much the same way `async/await` allows you to write code with promises in a more sequential way.
+/**
+ * Takes a generator function, in which parsers are `yield`ed. `coroutine` allows you to write parsers in a more imperative and sequential way ─ in much the same way `async/await` allows you to write code with promises in a more sequential way.
  * 
  * Inside of the generator function, you can use all regular Javascript and Typescript language features, like loops, variable assignments, and conditional statements. This makes it easy to write very powerful parsers using `coroutine`, but on the other side it can lead to less readable, more complex code.
  * 
@@ -139,7 +141,8 @@ export const coroutine = <T extends PStream<any>, D, R>(g: () => ParserMonad<T, 
     }
   }) as Parser<T, D, R>;
 
-/** Takes a positive number and returns a function. That function takes a parser and returns a new parser which matches the given parser **exactly** the specified number of times.
+/**
+ * Takes a positive number and returns a function. That function takes a parser and returns a new parser which matches the given parser **exactly** the specified number of times.
  * 
  * If that number is set to 0, then it will literally do nothing; however, the error will still be mapped.
  */
@@ -263,3 +266,68 @@ export const namedSequence = <T extends PStream<any>, D, R extends any[]>(paired
 
     return nextState.resultify(results);
   }) as Parser<T, D, PairedResults<R>>;
+
+/**
+ * Takes a separating parser then a sequence of parsers, and constructs a new parser that will match that sequence **interspersed** with the separating parser.
+ * 
+ * However, the resulting array will not contain the results matched by the separator parser.
+ */
+export const intersperse = <T extends PStream<any>, D, R extends any[], S>(separator: Parser<T, D, S>) =>
+  (parsers: ParserTuple<T, D, R>) =>
+  new Parser(s => {
+    if (s.error) return s;
+    const results: R[] = [];
+    let start = true;
+    for (const parser of parsers) {
+      if (!start) {
+        s = separator.pf(s);
+        if (s.error) return s;
+      }
+      s = parser.pf(s);
+      if (s.error) return s;
+      results.push(s.result as R[number]);
+      start = false;
+    }
+    return s.resultify(results);
+  }) as Parser<T, D, R>;
+
+/**
+ * Takes a separating parser then a value parser, and returns a new parser that will match the value parser **zero or more** times, **separated** by the separating parser.
+ * 
+ * However, the resulting array will not contain the results matched by the separator parser.
+ */
+export const sep = <T extends PStream<any>, D, R, S>(separator: Parser<T, D, S>) =>
+  (parser: Parser<T, D, R>) =>
+  new Parser(s => {
+    if (s.error) return s;
+    const results: R[] = [];
+    while (true) {
+      s = parser.pf(s);
+      if (s.error)
+        return results.length === 0 ? s.resultify(results) : s;
+      results.push(s.result as R);
+      s = separator.pf(s);
+      if (s.error) break;
+    }
+    return s.resultify(results);
+  }) as Parser<T, D, R[]>;
+
+/**
+ * Takes a separating parser then a value parser, and returns a new parser that will match the value parser **one or more** times, **separated** by the separating parser.
+ * 
+ * However, the resulting array will not contain the results matched by the separator parser.
+ */
+export const sep1 = <T extends PStream<any>, D, R, S>(separator: Parser<T, D, S>) =>
+  (parser: Parser<T, D, R>) =>
+  new Parser(s => {
+    if (s.error) return s;
+    const out = sep(separator)(parser).pf(s);
+    if (out.error) return out;
+    const length = out.result?.length ?? 0;
+    return length > 0
+      ? out
+      : s.errorify(new ParsingError({
+          from: "sep1",
+          expected: "to match at least one separated value"
+        }));
+  }) as Parser<T, D, R[]>;
