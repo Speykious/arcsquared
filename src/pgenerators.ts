64134c25fbc0a0a124402f183881fd7080378ecb
Speykious <speykious@gmail.com>
2021-02-15 04:27:09 +0100

    Implemented peek and str parser generators

--- src/pgenerators.ts
+++ src/pgenerators.ts
@@ -1,11 +1,11 @@
 import { charLength } from "./helpers";
-import { EOF } from "./constants";
+import { EOF, UEOF } from "./constants";
 import Parser from "./Parser";
 import ParsingError from "./ParsingError";
-import StringPStream from "./StringPStream";
+import StringPStream, { encoder } from "./StringPStream";
 
 /** Takes a character and returns a parser that matches that character **exactly once**. */
-export const char = (c: string): Parser<StringPStream, null, number> => {
+export const char = (c: string) => {
   if (!c || charLength(c) !== 1)
     throw new TypeError(`[char] must be called with a single character, got '${c}'`);
   
@@ -33,7 +33,7 @@ export const char = (c: string): Parser<StringPStream, null, number> => {
       expected: `character '${c}'`,
       actual: EOF
     }));
-  }) as Parser<StringPStream, null, number>;
+  }) as Parser<StringPStream, null, string>;
 }
 
 /** Matches **exactly one** utf8 character. */
@@ -53,4 +53,45 @@ export const anyChar = new Parser(s => {
     expected: "any character",
     actual: EOF
   }));
-}) as Parser<StringPStream, null, number>;
+}) as Parser<StringPStream, null, string>;
+
+/** Matches **exactly one** stream element without consuming any input. */
+export const peek = new Parser(s => {
+  if (s.error) return s;
+  const { index, target } = s;
+  const length = target.length;
+  return index < length
+    ? s.resultify(target.elementAt(index))
+    : s.errorify(new ParsingError({
+        index,
+        message: UEOF,
+      }));
+});
+
+/** Takes a string and returns a parser that matches that string **exactly once**. */
+export const str = (cs: string) => {
+  if (!(cs && typeof cs === "string"))
+    throw new TypeError(`[str] must be called with a string with strict positive length, got ${cs}`);
+  const es = encoder.encode(cs);
+  return new Parser(s => {
+    if (s.error) return s;
+    const { index, target } = s;
+    const remainingBytes = target.length - index;
+    if (remainingBytes < es.byteLength)
+      return s.errorify(new ParsingError({
+        from: "str",
+        index,
+        expected: `"${cs}"`,
+        actual: EOF
+      }));
+    const sai = target.getString(index, es.byteLength);
+    return cs === sai
+      ? s.resultify(target.nextChars(es.byteLength))
+      : s.errorify(new ParsingError({
+          from: "str",
+          index,
+          expected: `"${cs}"`,
+          actual: `"${sai}..."`
+        }));
+  }) as Parser<StringPStream, null, string>;
+};
