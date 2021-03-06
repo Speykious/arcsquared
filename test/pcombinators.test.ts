146d6187288d830429fafabbe9c8039f45de31d8
Speykious <speykious@gmail.com>
2021-03-06 21:20:11 +0100

    formatting and withData test

--- test/pcombinators.test.ts
+++ test/pcombinators.test.ts
@@ -1,5 +1,13 @@
 import { insp } from "../src/helpers";
-import { getData, ParsingError, setData, StringPStream } from "../src/index";
+import Parser, {
+  getData,
+  ParsingError,
+  setData,
+  str,
+  StringPStream,
+  strparse,
+  withData
+} from "../src/index";
 insp("illusion");
 
 describe("Parser combinators", () => {
@@ -30,11 +38,11 @@ describe("Parser combinators", () => {
     });
 
     const state3 = getData.pf(
-      state
-      .dataify("a")
-      .errorify(new ParsingError({
-        message: "dummy"
-      }))
+      state.dataify("a").errorify(
+        new ParsingError({
+          message: "dummy"
+        })
+      )
     );
     it("should not get data of failing state", () => {
       expect(state3).toMatchObject({
@@ -46,31 +54,31 @@ describe("Parser combinators", () => {
           message: "dummy"
         },
         result: null
-      })
+      });
     });
   });
 
   describe("setData", () => {
     // I played Undertale recently. I named my character AAAA.
-    const parser = setData("AAAA! Stay determined.");
+    const parser = setData("AAAA! Stay determined...");
     const state = parser.parse(new StringPStream("Determination."));
     it("should set data to a successful state", () => {
       expect(state).toMatchObject({
         target: {
           index: 0
         },
-        data: "AAAA! Stay determined.",
+        data: "AAAA! Stay determined...",
         error: null,
         result: null
       });
     });
 
     const state2 = parser.pf(
-      state
-      .dataify(null)
-      .errorify(new ParsingError({
-        message: "dummy"
-      }))
+      state.dataify(null).errorify(
+        new ParsingError({
+          message: "dummy"
+        })
+      )
     );
     it("should not set data to a failing state", () => {
       expect(state2).toMatchObject({
@@ -80,6 +88,38 @@ describe("Parser combinators", () => {
         data: null,
         error: {
           message: "dummy"
+        },
+        result: null
+      });
+    });
+  });
+
+  describe("withData", () => {
+    const innerParser = str("Where") as unknown as Parser<StringPStream, string, string>;
+    const parser = withData(innerParser)("kowa");
+    
+    it("should have the data when successful", () => {
+      const state = strparse(parser)("Where are the knives.");
+      expect(state).toMatchObject({
+        target: {
+          index: 4 // Wait it's not 5???
+        },
+        data: "kowa",
+        error: null,
+        result: "Where"
+      });
+    });
+
+    it("should still have the data when unsuccessful", () => {
+      const state = strparse(parser)("You're going to have a bad time.");
+      expect(state).toMatchObject({
+        target: {
+          index: 0
+        },
+        data: "kowa",
+        error: {
+          expected: '"Where"',
+          actual: '"You\'r..."'
         },
         result: null
       });
